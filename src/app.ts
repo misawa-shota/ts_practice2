3854d7f515322886c7c02c14507f9103de91a929
misawa-shota <kaiyou0120@gmail.com>
2026-01-06 00:00:01 +0900

    メモアプリ

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-import { Item } from "./item";
+function memo(): void {
+  var memo: unknown = prompt("入力してください");
+  var newElement = document.createElement("li");
+  newElement.textContent = String(memo);
 
-const elem = document.getElementById("output");
-var aBook = new Item('JavaScript本', 2500);
-aBook.say(elem);
+  var memo_list = document.getElementById("memo-list");
+  memo_list?.appendChild(newElement);
+}
+
+var btn = document.querySelector("button");
+btn?.addEventListener("click", memo);
