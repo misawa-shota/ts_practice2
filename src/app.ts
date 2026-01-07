a3d29f5e7a23674ce1b866fabee8429394b084be
misawa-shota <kaiyou0120@gmail.com>
2026-01-07 23:57:42 +0900

    TaskFormクラスの作成

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-function memo(): void {
-  var memo: unknown = prompt("入力してください");
-  var newElement = document.createElement("li");
-  newElement.textContent = String(memo);
+class TaskForm {
+  element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLTextAreaElement;
 
-  var memo_list = document.getElementById("memo-list");
-  memo_list?.appendChild(newElement);
+  constructor() {
+    // form要素を取得
+    this.element = document.querySelector('#task-form')!; // 非nullアサーション演算子
+
+    // input要素を取得
+    this.titleInputEl = document.querySelector('#form-title')!;
+    this.descriptionInputEl = document.querySelector('#form-description')!;
+    this.bindEvents();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault(); // フォームのデフォルトの送信動作を防止
+
+    // 確認用の処理（各入力項目のプロパティにアクセス）
+    console.log(this.titleInputEl.value);
+    console.log(this.descriptionInputEl.value);
+  }
+
+  private bindEvents() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
 }
 
-var btn = document.querySelector("button");
-btn?.addEventListener("click", memo);
+new TaskForm();
